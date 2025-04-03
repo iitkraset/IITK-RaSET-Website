58392d2c8c2d9d82bc4335c859410a924cf9708f
IITK RaSET <prabhusafi96277@gmail.com>
2025-04-04 02:29:31 +0530

    Update ScroolCon.js

--- assets/js/ScroolCon.js
+++ assets/js/ScroolCon.js
@@ -6,8 +6,10 @@
     })
     
     document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () =>{
+        
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
+        
     }))
        
     
