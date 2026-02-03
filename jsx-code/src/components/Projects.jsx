bb45498a2e53ab531c1854754ae31105190bc6c1
IITK RaSET <raset@iitk.ac.in>
2026-02-04 00:49:45 +0530

    Update Projects.jsx
    Tried something

--- jsx-code/src/components/Projects.jsx
+++ jsx-code/src/components/Projects.jsx
@@ -24,40 +24,17 @@ const Projects = () => {
               <a>Project June<i className="fa-solid fa-up-right-from-square fa-2xs" style={{marginLeft: '2rem'}}></i></a>
             </h1>
             <p style={{color: 'white', fontSize: '16px', fontWeight: 365}}>
-              <span className="specify">Made in collaboration with Indian Rocketry Association (IRA) </span> <br />
               <span className="specify">Motor Class : K95% </span><br />
               <span className="specify">Total Impulse : 2500 N·s</span> <br />
               <span className="specify">Maximum Thrust : 2700 N</span> <br />
               <span className="specify">Status : Successfully tested</span> <br />
+              <span className="specify">Project Lead: Prabhu Safi </span> <br />
               With a peak thrust of 2700 N and a total impulse of 2500 N·s, it stands as one of the most 
               powerful rocket motors built and successfully tested by a student team in India.
               Achieving this feat required advanced propellant optimization, precision manufacturing, and innovative design.
             </p>
           </div>
         </div>
-
-        {/* <div className="wrapper">
-          <div className="profile">
-            <img src="images/projects/Sagra.jpeg" alt="proj2" />
-          </div>
-          <div className="intro">
-            <h1 style={{fontWeight: 550, marginBottom: '12px'}}>
-              <a>Project Sagarmatha<i className="fa-solid fa-up-right-from-square fa-2xs" style={{marginLeft: '2rem'}}></i></a>
-            </h1>
-            <p style={{color: 'white', fontSize: '16px', fontWeight: 365}}>
-              Student-Developed High-Power Bi-Propellant Liquid Rocket Engine <br />
-              <span className="specify">Project Lead : Prabhu Safi</span> <br />
-              <span className="specify">Propellant : N₂O(L) / C₂H₅OH(L)</span><br />
-              <span className="specify">Thrust : 7000 N</span> <br />
-              <span className="specify">Specific Impulse : 216 s</span> <br />
-              <span className="specify">Burn time : 10 sec</span> <br />
-              <span className="specify">Cooling System : Regenerative cooling</span> <br />
-              <span className="specify">Status : Ongoing</span> <br />
-              This engine will produce 7 kN of thrust with a burn time of 10 seconds, powered by Liquid Nitrous Oxide (N₂O) as the oxidizer and Ethanol as the fuel, using a pressure-fed system. 
-              This initiative represents a pioneering effort in advancing liquid propulsion technology at the student level, a field known for its precision, efficiency, and potential for reusability. 
-            </p>
-          </div>
-        </div> */}
 
         <div className="wrapper">
           <div className="profile">
@@ -102,6 +79,7 @@ const Projects = () => {
             </p>          </div>
         </div>
 
+        <h2> PREVIOUS PROJECTS </h2>
         <div className="wrapper">
           <div className="profile">
             <img src="images/q-01.jpg" alt="proj5" />
